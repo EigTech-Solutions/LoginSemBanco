dc448fc8d8fec20943aa30e8a40a0035786357f6
PedroPradoCho <pedro.araujo@sptech.school>
2023-09-04 18:08:49 -0300

    lista dos acessos com email e senha de cada integrante

--- validacao-pi/src/main/java/Validacao.java
+++ validacao-pi/src/main/java/Validacao.java
@@ -7,18 +7,30 @@ public class Validacao {
         Scanner leitor = new Scanner(System.in);
 
         List<String> usuarios = new ArrayList<>();
-        usuarios.add("Pedro Prado");
+        usuarios.add("pedro.araujo@eigtech.com");
+        usuarios.add("inacio.figueiredo@eigtech.com");
+        usuarios.add("enzo.stane@eigtech.com");
+        usuarios.add("kauanny.felix@eigtech.com");
+        usuarios.add("caua.ribeiro@eigtech.com");
+        usuarios.add("joao.noleto@eigtech.com");
 
         List<String> senhas = new ArrayList<>();
-        senhas.add("123");
+        senhas.add("1301");
+        senhas.add("0508");
+        senhas.add("2206");
+        senhas.add("2411");
+        senhas.add("2301");
+        senhas.add("1303");
+
+        Boolean logar = false;
 
         System.out.println("""
                 ------------------------------
                 |          EigTech           |
                 ------------------------------
-                | Digite o usuário:          |
+                | Digite seu email:          |
                 """);
-        String user = leitor.nextLine();
+        String user = leitor.nextLine().toLowerCase();
 
         System.out.println("""
                 ------------------------------
@@ -27,20 +39,23 @@ public class Validacao {
         String senha = leitor.nextLine();
 
         for (int i = 0; i < usuarios.size(); i++) {
-
             if(user.equals(usuarios.get(i)) && senha.equals(senhas.get(i))){
-                System.out.println("""
+                logar = true;
+            }
+        }
+
+        if(logar){
+            System.out.println("""
                     ------------------------------
                     |        Bem Vindo           |
                     ------------------------------
                     """);
-            } else {
-                System.out.println("""
+        } else {
+            System.out.println("""
                     ------------------------------
                     |Usuário ou senha incorretos |
                     ------------------------------
                     """);
-            }
         }
     }
 }
